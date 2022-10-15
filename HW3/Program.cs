91b946863c1b72ad10abc5a0f142cd69b39055f4
Дмитро Єремія <diameremia2006@gmail.com>
2022-10-15 12:50:20 +0300

    Add project files.

---
+++ HW3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] a = { 1, -1, 0, 2, 3, -4, 5, 4, -8, 7, 11 };
+        Array.Sort(a);
+        foreach (int i in a)
+            if(i != 0)
+            Console.WriteLine(i);
+    
+    }
+}
